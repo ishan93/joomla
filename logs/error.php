613f7ae510fe4832599e3f31a9a8cced2f904b53
Ishan <ishanweragoda@gmail.com>
2016-11-18 16:04:21 +0530

    first

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-05-24 07:09:19 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-05-24	07:09:19	INFO	::1	Joomla FAILURE: 	Empty password not allowed
